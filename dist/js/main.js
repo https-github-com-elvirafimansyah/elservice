9d15d637c87291b6ed86fe9193e344c9ab96825a
elvirafirmansyah <freddyfirmansyah25@gmail.com>
2023-02-25 20:46:27 +0700

    feat: kalender

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,3 +7,50 @@ document.addEventListener("scroll", function () {
     navbar.classList.remove(["bg-white"], ["shadow-card"]);
   }
 });
+
+// const activePage = window.location.pathname;
+// const allLinks = document.querySelectorAll("nav ul li a").forEach(link => {
+//   console.log(link.href);
+//   if (link.href.includes(`${activePage}` + '#portfolio')){
+//     link.classList.remove('md:bg-transparent');
+//     console.log('sewot');
+//   } else if (link.href.includes(`${activePage}` + '#contact')){
+//     link.classList.remove('md:bg-transparent');
+//     // console.log('sewot');
+//   } else {
+
+//   }
+
+// });
+// console.log(activePage)
+const allLinks = document.querySelectorAll('nav ul li a');
+const allSection = document.querySelectorAll('section');
+console
+
+// window.onscroll = () => {
+//   allSection.forEach((section) => {
+//     let top = window.pageYOffset;
+//     let offset = section.offsetTop;
+//     let height = section.offsetHeight;
+//     let id = section.getAttribute('id');
+//     if (top >= offset && top < offset + height) {
+//       console.log(id);
+//       if (top >= offset && top < offset + height) {
+//         allLinks.forEach((link) => {
+//           console.log(top, offset);
+//           link.classList.remove('text-gray-700');
+//           link.classList.add('text-darkcolor');
+//           document.querySelector('nav ul li a[href=' + id + ']').classList.remove('text-gray-700');
+//           document.querySelector('nav ul li a[href=' + id + ']').classList.remove('text-darkcolor');
+//         })
+//       }
+//       // allLinks.forEach((link) => {
+//       //   const same = link.href.includes(id);
+//       //   if (id === same) {
+//       //     console.log('fuck')
+//       //   }
+//       //   document.querySelector('nav ul li a[href=' + id + ']').classList.add('text-darkcolor');
+//       // })
+//     }
+//   })
+// }
