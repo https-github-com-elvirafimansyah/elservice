bfec51eba1a633160993fda05bd468fe875ae6b6
elvirafimansyah <freddyfirmansyah25@gmail.com>
2023-09-07 12:10:19 +0700

    fix: image & link website page

--- dist/js/website.js
+++ dist/js/website.js
@@ -3,11 +3,11 @@ import { portoUI,portoWebUI, imgModal } from './templatePorto.js'
 const data_gambar = [
   {
     id: "1",
-    url: "../assets/portfolio/landingpage/eldesign2.png",
+    url: "../assets/portfolio/landingpage/eldesign1.png",
     category: "website",
     type: "",
     theme: "",
-    link: "https://eldesign2.netlify.app/",
+    link: "https://eldesign1.netlify.app/",
     note: 'Selamat pagi/siang/malam, saya mau pesan/order desain amplop - tema Graphel?'
   },
   {
@@ -21,29 +21,29 @@ const data_gambar = [
   },
   {
     id: "3",
-    url: "../assets/portfolio/landingpage/eldesign5.png",
-    category: "website",
-    type: "",
-    theme: "",
-    link: "https://eldesign5.netlify.app/",
-    note: 'Selamat pagi/siang/malam, saya mau pesan/order desain banner - tema Cipher?'
-  },
-  {
-    id: "4",
     url: "../assets/portfolio/landingpage/eldesign2.png",
     category: "website",
     type: "",
     theme: "",
     link: "https://eldesign2.netlify.app/",
+    note: 'Selamat pagi/siang/malam, saya mau pesan/order desain banner - tema Cipher?'
+  },
+  {
+    id: "4",
+    url: "../assets/portfolio/landingpage/eldesign5.png",
+    category: "website",
+    type: "",
+    theme: "",
+    link: "https://eldesign5.netlify.app/",
     note: 'Selamat pagi/siang/malam, saya mau pesan/order desain brosur - tema Empathy?'
   },
   {
     id: "5",
-    url: "../assets/portfolio/landingpage/eldesign4.png",
+    url: "../assets/portfolio/landingpage/eldesign3.png",
     category: "website",
     type: "",
     theme: "",
-    link: "https://eldesign4.netlify.app/",
+    link: "https://eldesign3.netlify.app/",
     note: 'Selamat pagi/siang/malam, saya mau pesan/order desain kalender - tema Azure?'
   }
 ]
